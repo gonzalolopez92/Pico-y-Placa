f5d6e07cbacf893bf91e05ec65af44a368dca015
Gonzalo lopez <gonzalo.lopez@outlook.com.ar>
2019-09-19 17:17:59 -0300

    consultants adapter added

--- app/src/main/java/app/mobile/picopalaapp/view/ConsultantsActivity.java
+++ app/src/main/java/app/mobile/picopalaapp/view/ConsultantsActivity.java
@@ -1,18 +1,29 @@
 package app.mobile.picopalaapp.view;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.List;
+
 import app.mobile.picopalaapp.R;
 import app.mobile.picopalaapp.controller.Controller;
+import app.mobile.picopalaapp.model.Consultant;
+import app.mobile.picopalaapp.view.adapters.ConsultantListAdapter;
 
 public class ConsultantsActivity extends AppCompatActivity {
 
     private Context context;
     private Controller controller;
+    private List<Consultant> consultantList;
+    private ListView lvConsultants;
+    private ConsultantListAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +33,51 @@ public class ConsultantsActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        lvConsultants = findViewById(R.id.lvConsultants);
+
         context = ConsultantsActivity.this;
         controller = new Controller(context);
 
+        new GetConsultants().execute();
+
+    }
+
+
+
+    private class GetConsultants extends AsyncTask<Void, Void, Void> {
+
+        private ProgressDialog dialog;
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = new ProgressDialog(context);
+            dialog.setTitle(getString(R.string.app_name));
+            dialog.setCancelable(false);
+            dialog.setIcon(R.mipmap.ic_launcher);
+            dialog.setMessage("Espere un momento...");
+            dialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                consultantList = controller.getConsultants();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            adapter = new ConsultantListAdapter(consultantList, context);
+            lvConsultants.setAdapter(adapter);
+            dialog.dismiss();
+
+        }
     }
 }
---
+++ app/src/main/java/app/mobile/picopalaapp/view/adapters/ConsultantListAdapter.java
@@ -0,0 +1,80 @@
+package app.mobile.picopalaapp.view.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import app.mobile.picopalaapp.R;
+import app.mobile.picopalaapp.model.Consultant;
+
+
+/**
+ * Created by usrkits on 13/4/2017.
+ */
+public class ConsultantListAdapter extends BaseAdapter {
+
+    private List<Consultant> consultantList;
+    private Context context;
+
+    public ConsultantListAdapter(List<Consultant> consultantList, Context context) {
+        this.consultantList = consultantList;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return consultantList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return consultantList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    private static class ViewHolder {
+        TextView tvDateRegister;
+        TextView tvLicensePlate;
+        TextView tvDateInput;
+        TextView tvIsCounterversion;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final Consultant consultant = consultantList.get(position);
+
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            convertView = inflater.inflate(R.layout.item_consultant, parent, false);
+
+            viewHolder = new ViewHolder();
+            viewHolder.tvDateRegister = convertView.findViewById(R.id.tvDateRegister);
+            viewHolder.tvLicensePlate = convertView.findViewById(R.id.tvLicensePlate);
+            viewHolder.tvDateInput = convertView.findViewById(R.id.tvDateInput);
+            viewHolder.tvIsCounterversion = convertView.findViewById(R.id.tvIsCounterversion);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.tvDateRegister.setText(consultant.getDateRegister());
+        viewHolder.tvLicensePlate.setText(consultant.getLicensePlate());
+        viewHolder.tvDateInput.setText(consultant.getDateConsultant());
+        viewHolder.tvIsCounterversion.setText(consultant.isCounterversion() == 1 ? "SI" : "NO");
+
+        return convertView;
+    }
+
+
+}
